15b0cef51306a803dab15907d44db918f71e2191
jchan555s <jchan555@shaw.ca>
2014-07-21 18:15:47 -0700

    fixed behaviour of menus so no 2 menus are dropped

--- code/jason-work/AddingDropdownMenus/menu.js
+++ code/jason-work/AddingDropdownMenus/menu.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
 
+  /* click on lecture menu */
   $("#dropdownbox-lecture").click(function(){
+  
+    // reset other menus
+	$("#exercise-menu").removeClass("showMenu-exercise");
+	$("#quiz-menu").removeClass("showMenu-quiz");
+	$("#courseinfo-menu").removeClass("showMenu-courseinfo");
+	
     $("#lecture-menu").toggleClass("showMenu-lecture");
       $("#lecture-menu > li").click(function(){
         $("#dropdownbox-lecture > p").text($(this).text());
-        $(".menu").removeClass("showMenu-lecture");
+        $("#lecture-menu").removeClass("showMenu-lecture");
       });
   });
   
 
+  /* click on Exercise Menu */
   $("#dropdownbox-exercise").click(function(){
+	// reset other menus
+	$("#lecture-menu").removeClass("showMenu-lecture");	
+	$("#quiz-menu").removeClass("showMenu-quiz");
+	$("#courseinfo-menu").removeClass("showMenu-courseinfo");
+  
     $("#exercise-menu").toggleClass("showMenu-exercise");
       $("#exercise-menu > li").click(function(){
         $("#dropdownbox-exercise > p").text($(this).text());
@@ -18,7 +31,14 @@ $(document).ready(function(){
   });
   
   
+  /* click on Quiz Menu */
   $("#dropdownbox-quiz").click(function(){
+  
+	// reset other menus.
+  	$("#lecture-menu").removeClass("showMenu-lecture");
+	$("#exercise-menu").removeClass("showMenu-exercise");	
+	$("#courseinfo-menu").removeClass("showMenu-courseinfo");
+	
     $("#quiz-menu").toggleClass("showMenu-quiz");
       $("#quiz-menu > li").click(function(){
         $("#dropdownbox-quiz > p").text($(this).text());
@@ -26,13 +46,26 @@ $(document).ready(function(){
       });
   });
   
+  /* click on Course Info menu */
   $("#dropdownbox-courseinfo").click(function(){
+    $("#lecture-menu").removeClass("showMenu-lecture");
+	$("#exercise-menu").removeClass("showMenu-exercise");
+	$("#quiz-menu").removeClass("showMenu-quiz");
+	
+	
     $("#courseinfo-menu").toggleClass("showMenu-courseinfo");
-      $("#courseinfo-menu > li").click(function(){
+      $("#courseinfo-menu > li").click(function(){	 	   		
         $("#dropdownbox-courseinfo > p").text($(this).text());
         $("#courseinfo-menu").removeClass("showMenu-courseinfo");
+					
       });
   });
   
 });
 
+function resetMenu(){
+	$("#lecture-menu").removeClass("showMenu-lecture");
+	$("#exercise-menu").removeClass("showMenu-exercise");
+	$("#quiz-menu").removeClass("showMenu-quiz");
+	$("#courseinfo-menu").removeClass("showMenu-courseinfo");
+}
